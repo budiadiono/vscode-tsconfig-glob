37ab9b1510609635c8dc889310c947c8019e7939
budiadiono <budiadiono@yahoo.com>
2016-04-18 11:29:31 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# tsconfig-glob
+## Generate files value on tsconfig.json based on filesGlob.
+
+You can specify a `filesGlob` pattern in your `tsconfig.json` file in root directory and this extension automatically generate list of files. 
+Yes just like in [atom-typescript](https://github.com/TypeStrong/atom-typescript/blob/master/docs/tsconfig.md) plugin when you are using **Atom** editor.
+
+### rewriteTsconfig
+You can disable this extension by set this option to `false` in your `tsconfig.json`:
+```
+{
+	"vscode" : {
+		"rewriteTsconfig" : false
+	}
+}
+```
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,62 @@
+var vscode = require('vscode');
+
+function activate(context) {
+    // Start cooking the tsconfig.json
+    vscode.workspace.openTextDocument(vscode.workspace.rootPath + '/tsconfig.json').then(start, quit);
+}
+
+function start(params) {
+    // Find tsconfig.json
+    var fs = require('fs');
+    var tsconfig = JSON.parse(fs.readFileSync(params.fileName));
+
+    // Ensure vscode.rewriteTsconfig option is not FALSE
+    var vcode = tsconfig.vscode;
+    if (vcode && vcode.rewriteTsconfig === false) {
+        return;
+    }
+
+    // Ensure filesGlob is defined
+    var filesGlob = tsconfig.filesGlob
+    if (!filesGlob || !filesGlob.length) {
+        return;
+    }
+
+    // Read filesGlob
+    var globs = [];
+    for (var index = 0; index < filesGlob.length; index++) {
+        var glb = filesGlob[index];
+        if (!glb.startsWith('!')) {
+            globs.push(glb)
+        }
+    }
+    if (!globs.length)
+        return;
+
+    // Watch files defined in filesGlob
+    var watcher = vscode.workspace.createFileSystemWatcher('{' + globs.join(',') + '}');
+    watcher.onDidChange(generateFiles)
+    watcher.onDidDelete(generateFiles)
+    watcher.onDidCreate(generateFiles);
+}
+
+function generateFiles() {
+    console.log('***GENERATE***');
+    var tsConfig = require("tsconfig-glob");
+    tsConfig({
+        configPath: vscode.workspace.rootPath,
+        cwd: process.cwd(),
+        indent: 2
+    });
+}
+
+function quit(params) {
+    vscode.window.showInformationMessage('File tsconfig.json was not found in your root directory.');
+}
+
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
